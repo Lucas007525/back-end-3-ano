b7bdab68cafc4175bd7a0b7c6dd25bf83d5fabf4
Lucas007525 <lucas.ferraz.barros@escola.pr.gov.br>
2026-04-08 11:53:41 -0300

    Add files via upload

---
+++ testes.js
@@ -0,0 +1,40 @@
+//1.Exiba a mensagem olá mundo no console.
+console.log ("Olá mundo"); 
+
+//2.Crie uma variável com seu nome e exiba o valor no console.
+
+let nome = "Lucas";
+console.log(nome);
+
+//3.Some dois números e exiba o resultado.
+
+let a = 1;
+let b = 2;
+let c = a + b;
+
+console.log(c);
+
+//4 Subtraia dois números e exiba valor.
+
+let sub = 4-2;
+console.log(sub);
+
+//5 Descubra o resto da divisão de 1 número por outro.
+
+let resto = 2 % 2;
+console.log (resto);
+
+// 6 Crie uma variável boleana e exiba seu valor.
+
+ let bu =  true;
+ console.log(bu);
+
+ // 7 Verifique se um número é maior que outro e exiba o resultado.
+
+let maiorQue= 18 > 11;
+
+console.log(maiorQue);
+
+
+
+
